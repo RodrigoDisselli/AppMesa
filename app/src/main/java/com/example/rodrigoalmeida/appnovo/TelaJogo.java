eccf7e8c6e3d9c8abcc762b7074f18a14d9913fc
Olaf <disselli.rodrigo@gmail.com>
2018-11-05 13:49:36 -0200

    first commit

---
+++ app/src/main/java/com/example/rodrigoalmeida/appnovo/TelaJogo.java
@@ -0,0 +1,13 @@
+package com.example.rodrigoalmeida.appnovo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaJogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_jogo);
+    }
+}
